2b4d1387be03a7ec6cfbd669cfa23fc104ee3450
Connor Wilson <connorwilson1@googlemail.com>
2015-12-24 22:50:05 +0000

    changed module introduction output

--- lib/web_gen.rb
+++ lib/web_gen.rb
@@ -1,13 +1,23 @@
 # Wrapper class for webgen, allows a user to generate a randomised vulnerable website using command line arguments
-require 'webgen/xml_reader'
-require 'webgen/webgen_engine'
-require 'webgen/puppet_controller'
-require 'webgen/vagrant_controller'
+require_relative 'webgen/xml_reader'
+require_relative 'webgen/webgen_engine'
+require_relative 'webgen/puppet_controller'
+require_relative 'webgen/vagrant_controller'
 
 class WebGen
   #set up environment variables
-  #display webgen ascii art to console, because if theres no ascii art then it isnt really a project is it
+  webgenConfig = 'location'
+  #display webgen ascii art to console, because if theres no ascii art then it isnt really a project.
+  text = File.open('../resources/webgen_ascii.txt').read
+  text.each_line do  |line|
+    puts line
+  end
 
-  #output environment variables
+  #make sure config locations exist, if one of them doesnt exist, exit program.
+  puts 'looking for webgen config in: ' + webgenConfig
+  if File.exist?(webgenConfig)
+    puts 'Config exists'
+  end
+
 
 end
