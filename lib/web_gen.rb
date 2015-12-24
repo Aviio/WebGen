fc9a909ab2ca4784c0cc07c6f86782b38e95a8da
Connor Wilson <connorwilson1@googlemail.com>
2015-12-24 21:12:16 +0000

    Project structure changes.

--- lib/web_gen.rb
+++ lib/web_gen.rb
@@ -1,3 +1,10 @@
 # Wrapper class for webgen, allows a user to generate a randomised vulnerable website using command line arguments
+require 'webgen/xml_reader'
+require 'webgen/webgen_engine'
+require 'webgen/puppet_controller'
+require 'webgen/vagrant_controller'
+
 class WebGen
+  #set up environment variables
+  #
 end
--- lib/webgen/xml_reader.rb
+++ lib/webgen/xml_reader.rb
@@ -1,3 +1,4 @@
 # Responsible for taking in an xml configuration and converting to an object that webgen_engine can use
+
 class XmlReader
 end
