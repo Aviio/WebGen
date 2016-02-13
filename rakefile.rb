896f6c41db86304d50520002b490f0132c0fb3c5
Connor <c.wilson4388@student.leedsbeckett.ac.uk>
2016-02-13 18:12:02 +0000

    Edit of rake file to run all tests under test/*/ directories

--- rakefile.rb
+++ rakefile.rb
@@ -6,5 +6,5 @@ Rake::TestTask.new do |t|
   t.libs = ['lib']
   t.warning = true
   t.verbose = true
-  t.test_files = FileList['tests/helpertests/*.rb']
+  t.test_files = FileList['tests/*/*.rb']
 end
